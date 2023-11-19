19cdbcb1972e6fa580441a902859f812a2c9946d
Sahasbhop <sahasbhop@gmail.com>
2023-11-20 00:19:21 +0700

    Increase enemy speed by 10 for each new wave

--- Assets/Challenge 4/Scripts/EnemyX.cs
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -2,23 +2,27 @@
 
 public class EnemyX : MonoBehaviour
 {
-    private const float Speed = 20;
+    public float speed;
     private Rigidbody _enemyRb;
     private GameObject _playerGoal;
+    private SpawnManagerX _spawnManager;
 
     // Start is called before the first frame update
     private void Start()
     {
         _enemyRb = GetComponent<Rigidbody>();
         _playerGoal = GameObject.Find("Player Goal");
+        _spawnManager = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>();
+        speed = CalculateSpeedForWave(_spawnManager.waveCount - 1);
     }
 
     // Update is called once per frame
+
     private void Update()
     {
         // Set enemy direction towards player goal and move there
         Vector3 lookDirection = (_playerGoal.transform.position - transform.position).normalized;
-        _enemyRb.AddForce(lookDirection * (Speed * Time.deltaTime));
+        _enemyRb.AddForce(lookDirection * (speed * Time.deltaTime));
     }
 
     private void OnCollisionEnter(Collision other)
@@ -32,5 +36,10 @@ public class EnemyX : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private float CalculateSpeedForWave(int waveCount)
+    {
+        return 10 + 10 * waveCount;
     }
 }
